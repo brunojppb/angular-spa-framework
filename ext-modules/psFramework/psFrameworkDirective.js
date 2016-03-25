8395b5e231017168668cf69ea9cb59a8e2a7f6df
Bruno Paulino <brunojppb@gmail.com>
2016-03-25 17:24:52 -0300

    Error handling

--- ext-modules/psFramework/psFrameworkDirective.js
+++ ext-modules/psFramework/psFrameworkDirective.js
@@ -5,10 +5,12 @@
       .directive('psFramework', function() {
         return {
           restrict: 'E',
-          transclusion: true,
+          transclusion: false,
           templateUrl: 'ext-modules/psFramework/psFrameworkTemplate.html',
           scope: {
-
+            title: "@",
+            subtitle: "@",
+            iconFile: "@"
           },
           controller: "psFrameworkController"
         };
