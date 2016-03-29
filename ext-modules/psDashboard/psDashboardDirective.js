904830c9b9cc8f749329a04418a8f5b66d341061
bruno paulino <brunojppb@gmail.com>
2016-03-29 09:40:28 -0300

    Add widget to dashboard dynamically

--- ext-modules/psDashboard/psDashboardDirective.js
+++ ext-modules/psDashboard/psDashboardDirective.js
@@ -6,7 +6,13 @@
     .module('psDashboard')
     .directive('psDashboard', function() {
       return {
-        templateUrl: 'ext-modules/psDashboard/psDashboardTemplate.html'
+        templateUrl: 'ext-modules/psDashboard/psDashboardTemplate.html',
+        link: function(scope, el, attrs, ctrl) {
+          scope.addNewWidget = function(widget) {
+            var newWidget = angular.copy(widget.settings);
+            scope.widgets.push(newWidget);
+          }
+        }
       };
     });
 
